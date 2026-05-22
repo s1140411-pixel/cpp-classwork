2679a73f89a69ef1edbab2cb98b0d5cf36d0ea59
s1140411-pixel <s1140411@dmhs.kh.edu.tw>
2026-05-22 10:54:19 +0800

    Create escape.cpp

---
+++ escape.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout<< "1.憂鬱道明" << endl;
+	cout<< "2.憂鬱 道明" << endl;
+	cout<< "3.憂鬱\t道明" << endl;
+	cout<< "4.憂鬱\n道明" << endl;
+	cout<< "\"5.憂鬱道明\"" << endl;
+	cout<< "\'6.憂鬱道明\'" << endl;
+	cout<< "7\\.憂鬱道明\\" << endl;
+	return 0;
+}
